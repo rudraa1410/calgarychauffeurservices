c69b7846bedccb5865cd052f42998c7ab5b180a4
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-12-17 18:15:37 -0700

    updated next.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Optional, for stricter React checks
 };
 
 export default nextConfig;
